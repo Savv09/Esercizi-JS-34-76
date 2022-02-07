e9005ff3cd610c9ff62346de2e4925b466b104d0
Savio Centola <saviocentola@gmail.com>
2022-02-07 16:18:31 +0100

    finito

---
+++ Esercizio 37/exercise-37.js
@@ -0,0 +1,23 @@
+class BankAccount {
+    constructor(initialBalance) {
+      this.balance = initialBalance;
+    }
+  
+    deposit(amount) {
+      this.balance += amount;
+    }
+  
+    withdraw(amount) {
+      this.balance -= amount;
+    }
+  
+    view() {
+      console.log(this.balance);
+    }
+}
+
+const bankAccount = new BankAccount(1000);
+bankAccount.deposit(500);
+bankAccount.deposit(200);
+bankAccount.withdraw(800);
+bankAccount.view();
---
+++ Esercizio 38/exercise-38.js
@@ -0,0 +1,45 @@
+class BankAccount {
+  #amount = 0;
+
+  constructor(initialAmount) {
+    this.#amount = initialAmount;
+  }
+
+  deposit(amount) {
+    this.#amount += amount;
+  }
+
+  withdraw(amount) {
+    this.#amount -= amount;
+  }
+
+  view() {
+    console.log(this.#amount);
+  }
+}
+
+class BankAccountVip extends BankAccount {
+  constructor(initialAmount) {
+    super(initialAmount)
+  }
+
+  deposit(amount) {
+    let newAmount = amount;
+
+    if (newAmount >= 1000) {
+      let sum = ((newAmount * 3)/ 100)
+      
+      newAmount += sum
+    }
+
+    super.deposit(newAmount)
+  }
+}
+
+
+const bankAccountVip = new BankAccountVip(1000);
+bankAccountVip.deposit(500);
+bankAccountVip.deposit(1200);
+bankAccountVip.withdraw(800);
+bankAccountVip.deposit(3500);
+bankAccountVip.view();
